935310acd7dc3593e3bae9413c629081887c2212
evo264714 <mh264714@gmail.com>
2023-01-29 23:58:49 +0600

    first

---
+++ includes.js
@@ -0,0 +1,2 @@
+let text = "Hello world, welcome to the universe.";
+text.includes("world");
---
+++ slice.js
@@ -0,0 +1,4 @@
+let fruits = ['apple', 'banana', 'cherry', 'date'];
+let selectedFruits = fruits.slice(1, 3);
+console.log(selectedFruits); // Output: ['banana', 'cherry']
+l

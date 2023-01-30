b28c6be9056aea379033bd93a74f7e6d320d7c02
evo264714 <mh264714@gmail.com>
2023-01-30 20:20:14 +0600

    done

---
+++ switch.js
@@ -0,0 +1,42 @@
+const color = 'yellow';
+if(color === 'green'){
+    console.log('This is green.');
+}
+else if (color === 'blue'){
+    console.log('This is blue.');
+}
+else if (color === 'red'){
+    console.log('This is red.');
+}
+else if (color === 'white'){
+    console.log('This is white.');
+}
+else if (color === 'yellow'){
+    console.log('This is yellow.');
+}
+else{
+    console.log('This is black');
+}
+
+//! SWITCH CASE
+
+switch(color){
+    case 'green':
+        console.log('This is green.')
+        break;
+    case 'blue':
+        console.log('This is blue.')
+        break;
+    case 'white':
+        console.log('This is white.')
+        break;
+    case 'red':
+        console.log('This is red.')
+        break;
+    case 'yellow':
+        console.log('This is yellow.')
+        break;
+    default:
+        console.log('This is green.')
+
+}

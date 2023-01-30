060b4118ec17f168e6a645c68ed4f5de86c414b6
evo264714 <mh264714@gmail.com>
2023-01-30 16:26:26 +0600

    para

---
+++ function.js
@@ -0,0 +1,13 @@
+//* Function declaration
+function startFan(){
+    console.log('Stand up');
+    console.log('walk towards the fan');
+    console.log('press the switch');
+}
+
+//* Call the function
+startFan();
+console.log('waking up');
+startFan();
+console.log('eating lunch');
+startFan();
---
+++ parameter.js
@@ -0,0 +1,19 @@
+function bringShingara(money){
+    console.log('Total money: ', money);
+    console.log('Ei nen Shingara');
+}
+var taka = 300;
+
+// bringShingara(taka);
+
+function add(num1, num2){
+    console.log('going to add: ', num1, num2);
+}
+// add(125, 96);
+
+function sum(a, b, c, d, e){
+    console.log(a, b, c, d, e);
+    var sum = a + b + c + d + e;
+    console.log(sum);
+}
+sum(15, 98, 56, 5, 9);

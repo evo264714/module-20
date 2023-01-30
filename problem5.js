38aa01d71d656e7953f347d9fb1df213bd87d6ec
evo264714 <mh264714@gmail.com>
2023-01-30 20:54:07 +0600

    done dpne

---
+++ problem5.js
@@ -0,0 +1,16 @@
+var signal = 'fsf';
+
+switch(signal){
+    case 'red':
+    console.log('Danger!');
+    break;
+    case 'yellow':
+    console.log('Stop!');
+    break;
+    case 'green':
+    console.log('Cross the road');
+    break;
+    default:
+        console.log('Go back home');
+
+}
